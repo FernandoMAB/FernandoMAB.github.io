4a29b45017d41971e2bdb1e14faff063c469d6b0
FernandoMAB <62525270+FernandoMAB@users.noreply.github.com>
2021-10-02 23:39:35 -0500

    Fire base prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyBvJaqWCiALeufjXeBrjznz8c4AzFo8yvQ",
+    authDomain: "ingwebfm.firebaseapp.com",
+    projectId: "ingwebfm",
+    storageBucket: "ingwebfm.appspot.com",
+    messagingSenderId: "657106277956",
+    appId: "1:657106277956:web:55e4a071088968f4ee0e11"
+  }
 };
